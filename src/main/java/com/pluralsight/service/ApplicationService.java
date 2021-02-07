883bacd3d2edafdc432ffa708d13c07f764c5b0a
Cagatay Erdiz <cagatay.erdiz@accenture.com>
2021-02-07 20:30:24 +0300

    Removed GraphQL, RESTful is back

---
+++ src/main/java/com/pluralsight/service/ApplicationService.java
@@ -0,0 +1,12 @@
+package com.pluralsight.service;
+
+import com.pluralsight.entity.Application;
+
+import java.util.List;
+
+public interface ApplicationService {
+    List<Application> listApplications();
+    Application findApplication(long id);
+}
+
+
