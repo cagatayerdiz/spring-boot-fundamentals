5c5a00552c9a728c3e5d867c407f8a208083b786
Cagatay Erdiz <cagatay.erdiz@accenture.com>
2021-02-07 18:33:28 +0300

    Created RESTful Web Api

--- src/main/java/com/pluralsight/service/ApplicationService.java
+++ src/main/java/com/pluralsight/service/ApplicationService.java
@@ -2,8 +2,11 @@ package com.pluralsight.service;
 
 import com.pluralsight.entity.Application;
 
+import java.util.List;
+
 public interface ApplicationService {
-    Iterable<Application> listApplications();
+    List<Application> listApplications();
+    Application findApplication(long id);
 }
 
 
